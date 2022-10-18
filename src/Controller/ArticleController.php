e488830f3923bccdda14411acc5367388562aac0
xayamanaka <k.yamanaka@mail.xearts.jp>
2022-10-19 01:12:25 +0900

    route

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,15 @@
+<?php
+namespace App\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage()
+    {
+        return new Response('OMG! My first page already! WOOO!');
+    }
+}
