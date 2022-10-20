ac3776f6e1e2288ba8e3d26b18e9ce1a736207f9
xayamanaka <k.yamanaka@mail.xearts.jp>
2022-10-20 15:34:04 +0900

    upade css

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,8 +2,9 @@
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -11,5 +12,24 @@ class ArticleController
     public function homepage()
     {
         return new Response('OMG! My first page already! WOOO!');
+
+    }
+
+    /**
+     * @Route("news/{slug}")
+     */
+    public function show($slug)
+    {
+        $comments = [
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
+        ];
+
+
+        return $this->render('article/show.html.twig',[
+            'title' => ucwords(str_replace('-',' ',$slug)),
+            'comments' => $comments,
+        ]);
     }
 }
